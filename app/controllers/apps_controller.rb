440f76c267de240e49cef57a92af35e4c3240c3e
Jan Scholich <jan@scholich.net>
2018-08-28 18:55:54 +0200

    incremental design changes

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,3 +1,12 @@
 class AppsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    if params[:category].present?
+      @apps = App.where(category: params[:category])
+    elsif params[:query].present?
+      @apps = App.all
+    else
+      @apps = App.all
+  end
 end
