28544be517e1ded3d65f3a3f75d9407bebc0a0df
Jack O'Connor <jack.oconnor@live.com>
2018-08-28 18:17:33 +0200

    html for the show page, added logo, added two buttons, added palceholder for screenshot, css for two different buttons

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,2 +1,18 @@
 class AppsController < ApplicationController
+  before_action :set_app, only: [:show]
+
+  def show
+  end
+
+
+  private
+
+  def app_params
+    params.require(:app).permit(:name, :description, :logo, :webpage_url)
+  end
+
+  def set_app
+    @app = App.find(params[:id])
+  end
+
 end
