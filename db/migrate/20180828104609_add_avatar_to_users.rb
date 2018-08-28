c779deb9fd0656c8543f4056d2edc47b9bf77a19
Jan Scholich <jan@scholich.net>
2018-08-28 16:26:30 +0200

    default avatar image

--- db/migrate/20180828104609_add_avatar_to_users.rb
+++ db/migrate/20180828104609_add_avatar_to_users.rb
@@ -1,5 +1,5 @@
 class AddAvatarToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :avatar, :string, default: ActionController::Base.helpers.asset_path("default_avatar.png")
+    add_column :users, :avatar, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2018_08_28_114050) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "avatar", default: "/assets/default_avatar-4797449c8f03f616359653337cc9405488db752bd98fa35d8825f12cc4093358.png"
+    t.string "avatar"
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
