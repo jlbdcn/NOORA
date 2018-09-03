d67b9d68038ac936633f308bd43373712a1ab9c8
julien <jlbdcn@gmail.com>
2018-09-03 18:37:20 +0200

    star system

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :app
   belongs_to :user
   validates :content, presence: true
-  validates :rating, inclusion: { in: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }, presence: true
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }, presence: true
 end
