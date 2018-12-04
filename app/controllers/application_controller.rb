be3419afcf15ff21e3b3d5f6b0dab60d54be1e04
Joao Torres <joaoatorres@gmail.com>
2018-12-04 01:34:32 -0200

    redirect after sign in and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,14 @@ class ApplicationController < ActionController::Base
     { host: ENV["HOST"] || "localhost:3000" }
   end
 
+  def after_sign_in_path_for(resource_or_scope)
+   apps_path
+  end
+
+  def after_sign_up_path_for(resource)
+    apps_path
+  end
+
   protected
 
   def configure_permitted_parameters
